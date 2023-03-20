a583c7ab232c556b3c63038ea00c37e808aea6f3
SeoulPolarBear <96487511+SeoulPolarBear@users.noreply.github.com>
2023-03-20 23:17:21 +0900

    if

---
+++ Go Lang/section3/if2.go
@@ -0,0 +1,24 @@
+//If문2
+
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 50
+	b := 70
+
+	//예제1
+	if a >= 65 {
+		fmt.Println("65 이상")
+	} else {
+		fmt.Println("65 이하")
+	}
+
+	//예제2
+	if b >= 70 {
+		fmt.Println("70 이상")
+	} else {
+		fmt.Println("70 미만")
+	}
+}
