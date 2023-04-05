3b08e61838191d2be4cf8532731f89c0001de9bd
SeoulPolarBear <96487511+SeoulPolarBear@users.noreply.github.com>
2023-04-05 23:21:07 +0900

    Delete Django/inflearn/Django/Django/section1/myenv directory

--- Django/inflearn/Django/Django/section1/myenv/myproject/onememos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-#default : localhost:8000/onememos/
-urlpatterns = [
-    path('', views.main, name = 'index'), # (router, view function) -> default IP : localhost:8000/onememos/
-    path('createMemo/',views.createMemo),
-]
