82f74a61642fb2893131c5f94f9aef00af261d2a
SeoulPolarBear <96487511+SeoulPolarBear@users.noreply.github.com>
2023-04-06 23:46:07 +0900

    Struct

---
+++ Go Lang/section8/struct3.go
@@ -0,0 +1,31 @@
+//구조체 기본3
+
+package main
+
+import "fmt"
+
+type car struct {
+	color string
+	name  string
+}
+
+func main() {
+	//예제1
+	c1 := car{"red", "220d"}
+	c2 := new(car)
+	c2.color, c2.name = "white", "sonata"
+	c3 := &car{}
+	c4 := &car{"black", "520d"}
+
+	fmt.Println(c1)
+	fmt.Println(c2)
+	fmt.Println(c3)
+	fmt.Println(c4)
+	fmt.Println()
+	/*
+		{red 220d}
+		&{white sonata}
+		&{ }
+		&{black 520d}
+	*/
+}
